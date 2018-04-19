80f2705fbeac04b54d4faf094fc7818ccac80796
Calvin Brownlee <calvinbrownlee@gmail.com>
2018-04-19 12:03:21 -0400

    bug: Fix pep8 style bugs

--- sort.py
+++ sort.py
@@ -3,47 +3,64 @@ import csv
 rate_data = []
 video_data = []
 
+# Please add videos.csv filepath
 video_path = "C:\\Users\\DrBrowntown\\Vimeo\\videos.csv"
+# Please add exchange_rates.csv filepath
 rate_path = "C:\\Users\\DrBrowntown\\Vimeo\\exchange_rates.csv"
+# Please add valid.csv filepath
 valid_path = "C:\\Users\\DrBrowntown\\Vimeo\\valid.csv"
+# Please add invalid.csv filepath
 invalid_path = "C:\\Users\\DrBrowntown\\Vimeo\\invalid.csv"
-#CSV reader for videos.csv
+
+# CSV reader for videos.csv
+
+
 def videos_csv_reader():
-    
     video_file = open(video_path, newline='')
     video_reader = csv.reader(video_file)
     video_header = next(video_reader)
-    
+
     for row in video_reader:
         id = int(row[0])
         title = str(row[1])
         total_likes = int(row[2])
         total_purchases = int(row[3])
-        unit_price_in_usd = float(row[4])    
+        unit_price_in_usd = float(row[4])
 
-        video_data.append([id, title, total_likes, total_purchases, unit_price_in_usd])
+        video_data.append([
+            id,
+            title,
+            total_likes,
+            total_purchases,
+            unit_price_in_usd
+            ])
 
-#CSV reader for exchange_rates.csv
+# CSV reader for exchange_rates.csv
+
+
 def exchange_rate_csv_reader():
-    
+
     rate_file = open(rate_path, newline='')
     rate_reader = csv.reader(rate_file)
     rate_header = next(rate_reader)
-    
+
     for row in rate_reader:
         currency = str(row[0])
         exchange_rate_from_usd = float(row[1])
 
         rate_data.append([currency, exchange_rate_from_usd])
 
+# Sorts the video.csv into two csv's of valid and invalid csv's
+
+
 def sort_valid_and_invalid_videos():
     total_cash = 0
-    # Define valid writer    
+    # Define valid writer
     valid_file = open(valid_path, 'w')
     valid_writer = csv.writer(valid_file)
     valid_writer.writerow(["Valid ID"])
 
-    # Define invalid writer    
+    # Define invalid writer
     invalid_file = open(invalid_path, 'w')
     invalid_writer = csv.writer(invalid_file)
     invalid_writer.writerow(["Invalid ID"])
@@ -55,13 +72,13 @@ def sort_valid_and_invalid_videos():
 
         if rate_currency == "EUR":
             EUR_exchange_rate = rate
-          
+
         elif rate_currency == "CAD":
             CAD_exchange_rate = rate
-   
+
     # Sort valid and invalid ID's
     for i in range(len(video_data)):
-        
+
         row_data = video_data[i]
         vid_id = row_data[0]
         vid_title = row_data[1]
@@ -70,22 +87,23 @@ def sort_valid_and_invalid_videos():
         vid_purchases_USD = row_data[4]
         vid_purchases_conversion = vid_purchases_USD * CAD_exchange_rate
 
-        #Filtering rules
-        if (len(vid_title) < 30 and 
-                vid_likes > 10 and 
-                vid_purchases > 200 and 
+        # Filtering rules
+        if (len(vid_title) < 30 and
+                vid_likes > 10 and
+                vid_purchases > 200 and
                 vid_purchases_conversion < 25):
-
-            total_cash += vid_purchases_USD 
-            valid_ID = vid_id         
-            valid_writer.writerow([valid_ID])      
-          
+            # Adds each valid video to the cash total and its ID to valid.csv
+            total_cash += vid_purchases_USD
+            valid_ID = vid_id
+            valid_writer.writerow([valid_ID])
+        # Videos taht fail the filtering are placed in invalid.csv
         else:
             invalid_ID = vid_id
             invalid_writer.writerow([invalid_ID])
 
-    print ("Total US dollar value of valid videos is $"+ str(round(total_cash)))
-            
+    print ("Total US dollar value of valid videos is ${0}"
+           .format(round(total_cash)))
+
 videos_csv_reader()
 exchange_rate_csv_reader()
 sort_valid_and_invalid_videos()
