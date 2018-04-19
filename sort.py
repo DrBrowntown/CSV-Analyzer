f3b80bd2bc03e73d5df85b1d8139bf7c614bb4e5
Calvin Brownlee <calvinbrownlee@gmail.com>
2018-04-19 16:16:24 -0400

    feat: Add error handling

--- sort.py
+++ sort.py
@@ -1,4 +1,6 @@
 import csv
+import errno
+import os
 
 rate_data = []
 video_data = []
@@ -16,45 +18,57 @@ invalid_path = "C:\\Users\\DrBrowntown\\Vimeo\\invalid.csv"
 
 
 def videos_csv_reader():
-    video_file = open(video_path, newline='')
-    video_reader = csv.reader(video_file)
-    video_header = next(video_reader)
+    try:
+        video_file = open(video_path, newline='')
+        video_reader = csv.reader(video_file, strict = True)
+        video_header = next(video_reader)
 
-    for row in video_reader:
-        id = int(row[0])
-        title = str(row[1])
-        total_likes = int(row[2])
-        total_purchases = int(row[3])
-        unit_price_in_usd = float(row[4])
+        for row in video_reader:
+            id = int(row[0])
+            title = str(row[1])
+            total_likes = int(row[2])
+            total_purchases = int(row[3])
+            unit_price_in_usd = float(row[4])
 
-        video_data.append([
-            id,
-            title,
-            total_likes,
-            total_purchases,
-            unit_price_in_usd
-            ])
+            video_data.append([
+                id,
+                title,
+                total_likes,
+                total_purchases,
+                unit_price_in_usd
+                ])
+
+    except FileNotFoundError as e:
+        print(e)
+    except ValueError as e:
+        print(
+            'Invalid value in videos.csv. Likes, Purchases, and Unit Price must be a float or integer. Error: ' 
+            + str(e))    
 
 # CSV reader for exchange_rates.csv
 
 
 def exchange_rate_csv_reader():
+    try:
+        rate_file = open(rate_path, newline='')
+        rate_reader = csv.reader(rate_file, strict = True)
+        rate_header = next(rate_reader)
 
-    rate_file = open(rate_path, newline='')
-    rate_reader = csv.reader(rate_file)
-    rate_header = next(rate_reader)
+        for row in rate_reader:
+            currency = str(row[0])
+            exchange_rate_from_usd = float(row[1])
 
-    for row in rate_reader:
-        currency = str(row[0])
-        exchange_rate_from_usd = float(row[1])
-
-        rate_data.append([currency, exchange_rate_from_usd])
-
+            rate_data.append([currency, exchange_rate_from_usd])
+    except FileNotFoundError as e:
+        print(e)    
 # Sorts the video.csv into two csv's of valid and invalid csv's
 
 
 def sort_valid_and_invalid_videos():
+
     total_cash = 0
+    CAD_exchange_rate = 0
+    vid_purchases_conversion = 0
     # Define valid writer
     valid_file = open(valid_path, 'w')
     valid_writer = csv.writer(valid_file)
@@ -64,6 +78,7 @@ def sort_valid_and_invalid_videos():
     invalid_file = open(invalid_path, 'w')
     invalid_writer = csv.writer(invalid_file)
     invalid_writer.writerow(["Invalid ID"])
+
     # Pull in conversion rates
     for i in range(len(rate_data)):
         ex_data = rate_data[i]
@@ -78,7 +93,7 @@ def sort_valid_and_invalid_videos():
 
     # Sort valid and invalid ID's
     for i in range(len(video_data)):
-
+        # Define the rows to be checked
         row_data = video_data[i]
         vid_id = row_data[0]
         vid_title = row_data[1]
@@ -101,8 +116,15 @@ def sort_valid_and_invalid_videos():
             invalid_ID = vid_id
             invalid_writer.writerow([invalid_ID])
 
-    print ("Total US dollar value of valid videos is ${0}"
-           .format(round(total_cash)))
+    # If vid_purchases_conversion is 0, the tool could not access exchange_rate        
+    if (vid_purchases_conversion == 0):
+            total_cash = 0
+            print('Unable to find currency conversion rate')
+    if (total_cash <= 0):
+        print("Unable to find any valid videos")
+    else:
+        print ("Total US dollar value of valid videos is ${0}"
+       .format(round(total_cash)))
 
 videos_csv_reader()
 exchange_rate_csv_reader()
